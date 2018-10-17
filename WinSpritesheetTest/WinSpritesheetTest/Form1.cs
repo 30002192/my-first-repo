cec6aef073bd2d80bc4a562e7371941e83bda680
30002192 <30002192@student.toiohomai.ac.nz>
2018-10-18 09:48:45 +1300

    testing visual studio

---
+++ WinSpritesheetTest/WinSpritesheetTest/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinSpritesheetTest
+{
+    public partial class Form1 : Form
+    {
+        SpriteSheetHandler sprite = new SpriteSheetHandler(@"H:\advnt_full.png", 32,65);
+        SpriteSheetHandler sprite2 = new SpriteSheetHandler(@"H:\advnt_full.png", 32, 65);
+        double tick;
+       int distance;
+        public Form1()
+        {
+            InitializeComponent();
+          
+            
+        }
+
+        private void FrameUpdate_Tick(object sender, EventArgs e)
+        {
+
+
+           
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            pb_char1.Image = sprite.curState;
+            pb_char2.Image = sprite2.curState;
+
+            sprite.InitialiseSprite(5,1,6, 0);
+            sprite2.InitialiseSprite(5,1, 6, 0);
+
+        }
+
+        private void StartAnimation(string animate)            
+        {
+
+
+            if (animate=="walk")
+            {
+                sprite.InitialiseSprite(5, 1, 6, 0);
+                  sprite2.InitialiseSprite(5, 1, 6, 0);
+            }
+            if (animate =="idle")
+            {
+                sprite.InitialiseSprite(5, 0, 0, 0);
+                sprite2.InitialiseSprite(5, 0, 0, 0);
+
+            }
+        }
+
+
+
+        private void Frame_Tick_Tick(object sender, EventArgs e)
+        {
+            pb_char1.Image = sprite.curState;
+            pb_char2.Image = sprite2.curState;
+            sprite.playSprite(false);          
+            sprite2.playSprite(true);
+
+
+
+
+            //
+            tick += 1;
+            label1.Text = Convert.ToString(tick);
+            //
+
+
+
+
+            if (CheckDistance()<-32)
+            {
+                pb_char2.Left -= 2;// moving pb right
+                pb_char1.Left += 2;
+            }
+            else
+            {
+                StartAnimation("idle");
+            }
+
+        }
+        private int CheckDistance()
+        {
+            distance = pb_char1.Location.X-pb_char2.Location.X;
+
+            return distance;
+        }
+
+        
+    }
+}
---
+++ WinSpritesheetTest/WinSpritesheetTest/SpriteSheetHandler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WinSpritesheetTest
+{
+   
+    class SpriteSheetHandler
+    {
+
+        //w: 32px h: 65px
+    
+      static  Bitmap sheet;
+       static int fWidth, fHeight;
+      static  int framesPerRow, framesPerCol;
+      static  int nextFrame;
+        static int endFrame;
+        static int row;
+        public Bitmap curState;
+        public Bitmap flipedState;
+        static int startF;
+        public SpriteSheetHandler(string path, int width, int height)
+        {
+            sheet = new Bitmap(path);
+            fWidth = width;
+            fHeight = height;
+            framesPerRow = sheet.Width / fWidth;
+            framesPerCol = sheet.Height / fHeight;
+            curState = new Bitmap(path);
+        }
+
+        public void InitialiseSprite(int frameSpeed, int startingFrame, int currentEndFrame, int curRow)
+        {
+            startF = startingFrame;
+            nextFrame = startingFrame ;
+            endFrame = currentEndFrame;
+            row = curRow;
+        }
+
+        public void playSprite(bool fliped)
+        {
+            
+
+
+            if (nextFrame<=endFrame)
+            {
+
+           
+                    Rectangle cloneRect = new Rectangle(nextFrame  * fWidth,row * fHeight, fWidth, fHeight);
+                    System.Drawing.Imaging.PixelFormat pFormat = sheet.PixelFormat;
+                     curState = sheet.Clone(cloneRect, pFormat);
+                nextFrame+=1;
+                if (fliped)
+                {
+                    curState.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                    flipedState = curState;
+                }
+                //System.Threading.Thread.Sleep(1000);
+            }
+            else
+            {
+                nextFrame = startF;
+            }
+            }
+        }
+    }
